0df4703f1b4820c8a270a5fafd624f54fcb730af
Andy Yu <aladidia@DESKTOP-7IJI918.localdomain>
2020-07-16 21:24:04 +1000

    trying to fix some bugs

--- frontend/src/apis/loginApi.js
+++ frontend/src/apis/loginApi.js
@@ -1,16 +1,9 @@
 import axios from "axios";
+import { valueFocusAriaMessage } from "react-select/src/accessibility";
 
-const loginApi = async (details) => {
-  let baseUrl = "http://127.0.0.1:5000/login";
-
-  function transformDetails(details) {
-    const transformedDetails = {
-      username: details.username,
-      password: details.password,
-    };
-    return transformedDetails;
-  }
-  const payload = { ingredients: details.map(transformDetails) };
+const loginApi = async (values) => {
+  let baseUrl = "http://127.0.0.1:5000/auth/login";
+  const payload = { username: values.username, password: values.password };
   console.log(payload);
   const config = {
     headers: {
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -12,7 +12,9 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-
+import { Formik } from "formik";
+import LoginForm from "./LoginForm";
+import loginApi from "../apis/loginApi";
 function Copyright() {
   return (
     <Typography
@@ -44,78 +46,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignIn() {
-  const classes = useStyles();
-
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-
-          {/* <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          /> */}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={(e) => {
-              console.log(e);
-
-              // const response = await loginApi(e)
-              // const data = response.data
-            }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            {/* <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid> */}
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={async (values) => {
+        console.log(values);
+        const response = await loginApi(values);
+        console.log(response);
+      }}
+    >
+      {(props) => <LoginForm {...props} />}
+    </Formik>
   );
 }

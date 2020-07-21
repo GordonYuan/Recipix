e0c91b53bfcd29f04a6ab7b392c8737903a0ac37
Andrew He <z5204847@unsw.edu.au>
2020-07-22 02:14:31 +1000

    implemented getting user_recipes

--- backend/api/recipe.py
+++ backend/api/recipe.py
@@ -15,7 +15,6 @@ class All(Resource):
     ''')
     def get(self):
         ### TODO
-        latest_recipes = ''
         conn = sqlite3.connect('database/recipix.db')
         c = conn.cursor()
         c.execute('SELECT * from recipes;')
@@ -163,10 +162,56 @@ class User(Resource):
     ''')
     def get(self):
         ### TODO
-        latest_recipes = ''
-        return {
-            'recipes': latest_recipes
-        }
+        user = authenticate(request)
+
+        conn = sqlite3.connect('database/recipix.db')
+        c = conn.cursor()
+        c.execute('SELECT * from recipes where username = ?', (user, ))
+        recipe_t = c.fetchall()
+        
+        ret = {"recipes" : []}
+        for i, t in enumerate(recipe_t):
+            c.execute('SELECT tag from Recipe_Tag where recipe_id = {}'.format(t[0]))
+            tag_t = c.fetchall()
+            c.execute('SELECT ingredient_name, amount, units from Recipe_Has where recipe_id = {}'.format(t[0]))
+            ingredient_t = c.fetchall()
+            c.execute('SELECT step, instruction from Methods where recipe_id = {}'.format(t[0]))
+            method_t = c.fetchall()
+
+            ret["recipes"].append({})
+            curr = ret["recipes"][i]
+            curr["recipe_id"] = t[0]
+            curr["recipe_creator"] = t[1]
+            curr["recipe_name"] = t[2]
+            curr["servings"] = t[3]
+            curr["description"] = t[4]
+            curr["image"] = t[5]
+
+            curr["tags"] = []
+            curr["ingredients"] = []
+            curr["method"] = []
+
+            for i, t in enumerate(tag_t):
+                curr["tags"].append({})
+                curr_tag = curr["tags"][i]
+                curr_tag["tag"] = t[0]    
+
+            for i, t in enumerate(ingredient_t):
+                curr["ingredients"].append({})
+                curr_ingred = curr["ingredients"][i]
+                curr_ingred["name"] = t[0]
+                curr_ingred["amount"] = t[1]
+                curr_ingred["units"] = t[2]
+            
+            for i, t in enumerate(method_t):
+                curr["method"].append({})
+                curr_method = curr["method"][i]
+                curr_method["step_number"] = t[0]
+                curr_method["instruction"] = t[1]
+        
+        c.close()
+        conn.close()
+        return ret
 
 @recipe.route('/request', strict_slashes=False)
 class Request(Resource):

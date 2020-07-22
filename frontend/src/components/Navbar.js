605d105d8ca37b779160348c4bfab39ea8a41736
Gordon Yuan <z5163243@unsw.edu.au>
2020-07-22 21:35:03 +1000

    removed console warnings

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import { withRouter } from "react-router";
@@ -23,7 +21,6 @@ const NavBar = ({ history }) => {
         <Grid
           justify="space-between" // Add it here :)
           container
-          spacing={24}
         >
           <Grid item>
             <img

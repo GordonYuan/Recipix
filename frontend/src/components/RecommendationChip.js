be76230a6dd3b685e24aae96b91a7f5e941315ff
Andy Yu <z5164075@student.unsw.edu.au>
2020-07-30 20:41:24 +1000

    tidying up

--- frontend/src/components/RecommendationChip.js
+++ frontend/src/components/RecommendationChip.js
@@ -3,13 +3,26 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Chip from "@material-ui/core/Chip";
 import { titleCase } from "../pages/HomePage";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "left",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
+}));
 
 const RecommendationChip = (props) => {
+  const classes = useStyles();
   const { ingredientsList, recommendations, setIngredients } = props;
   return (
-    <Grid container spacing={2}>
-      <Typography>Do you have?</Typography>
-      {recommendations &&
+    <div className={classes.root}>
+      {recommendations.length !== 0 && <Typography>Do you have?</Typography>}
+      {recommendations.length !== 0 &&
         recommendations.map((ingredient) => (
           <Chip
             size="small"
@@ -30,7 +43,7 @@ const RecommendationChip = (props) => {
             }}
           ></Chip>
         ))}
-    </Grid>
+    </div>
   );
 };
 

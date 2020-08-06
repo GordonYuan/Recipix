df72f21127bbbd53dd4023875e1e3cf0ed0c43ff
Gordon Yuan <z5163243@unsw.edu.au>
2020-08-06 15:48:53 +1000

    changed error message

--- frontend/src/pages/AuthErrorPage.js
+++ frontend/src/pages/AuthErrorPage.js
@@ -1,13 +1,7 @@
 import React from "react";
 
 const AuthErrorPage = () => {
-  return (
-    <h1>
-      ERROR: OOPSIE WOOPSIE!! Uwu You made a fucky wucky!! A wittle fucko
-      boingo! The code monkeys in your brain should be working VEWY HAWD to
-      figure out what you did wawng!
-    </h1>
-  );
+  return <h1>ERROR: User unauthorised</h1>;
 };
 
 export default AuthErrorPage;
